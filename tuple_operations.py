5077fc561c76c19270a64cb2f60e7d70812ca5b3
manju <manjuks155@gmail.com>
2024-05-16 08:56:57 +0530

    tuple and set operations

--- tuple_operations.py
+++ tuple_operations.py
@@ -2,9 +2,9 @@
 num_tuple = (20, 5, -8, 3, 18, 25)
 name_tuple = ("Java", "Python", "AWS")
 mixed_tuple = (2, 6, 4, -7, 'Manju', 10.5, True)
-tuple1 = (10)   # <class 'int'>     # Invalid tuple
+tuple1 = (10)  # <class 'int'>     # Invalid tuple
 tuple2 = (20,)  # valid tuple
-tuple3 = ()     # Empty tuple   # valid
+tuple3 = ()  # Empty tuple   # valid
 
 print(type(num_tuple))
 print(type(name_tuple))
@@ -13,5 +13,13 @@ print(type(tuple1))
 print(type(tuple2))
 print(type(tuple3))
 
+# list to tuple
+list_ex = [10, -5, 2, 7, 8, True, 'Python']
+print(list_ex)
+tuple_ex = tuple(list_ex)
+print("list to tuple : ")
+print(tuple_ex)
 
-
+list_t = list(tuple_ex)
+print("tuple to list : ")
+print(list_t)
